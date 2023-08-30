b4dffe0c4400268154f55dfd36e5c0c3a1688ba0
강남\82103 <kangng0806@gmail.com>
2023-08-31 01:55:55 +0900

    [과제 완료] jstl 반복문

--- src/main/java/com/uilangage/spring/test/jstl/JSTLController.java
+++ src/main/java/com/uilangage/spring/test/jstl/JSTLController.java
@@ -1,13 +1,34 @@
 package com.uilangage.spring.test.jstl;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/jstl")
 @Controller
 public class JSTLController {
 	
-	@GetMapping("/jstl/test01")
+	@GetMapping("/test01")
 	public String test01() {
 		return "jstl/test01";
+	}
+	
+	@GetMapping("/test02")
+	public String test02(Model model) {
+		
+		List<String> musicRanking = new ArrayList<>();
+		musicRanking.add("강남스타일");
+		musicRanking.add("벚꽃엔딩");
+		musicRanking.add("좋은날");
+		musicRanking.add("거짓말");
+		musicRanking.add("보고싶다");
+		
+		model.addAttribute("ranking", musicRanking);
+		
+		return "jstl/test02";
 	}
 }
