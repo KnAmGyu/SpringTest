59e08a81c0c202697f933fcb7c8325c599e6a769
강남\82103 <kangng0806@gmail.com>
2023-09-13 00:49:27 +0900

    [숙제] 완료

--- src/main/java/com/uilangage/spring/test/jpa/RecruitController.java
+++ src/main/java/com/uilangage/spring/test/jpa/RecruitController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.uilangage.spring.test.jpa.domain.Recruit;
 import com.uilangage.spring.test.jpa.repository.RecruitRepository;
 
-@RequestMapping("/jpa/recruit")
+@RequestMapping("/jpa/recruit/find")
 @Controller
 public class RecruitController {
 	
@@ -22,7 +22,7 @@ public class RecruitController {
 	
 	
 	// 1. id 로 조회
-	@GetMapping("/find/id")
+	@GetMapping("/id")
 	@ResponseBody
 	public List<Recruit> selectRecruitById(){
 		List<Recruit> recruitList = recruitRepository.findById(8);
@@ -32,7 +32,7 @@ public class RecruitController {
 	}
 	
 	// 2. Parameter 조건 조회
-	@GetMapping("/find/company")
+	@GetMapping("/company")
 	@ResponseBody
 	public List<Recruit> selectCompanyId(@RequestParam("companyId") int companyId){
 		List<Recruit> recruitList = recruitRepository.findAllBycompanyId(companyId);
@@ -42,12 +42,51 @@ public class RecruitController {
 	
 	// 3. 복합 조건 조회
 	//	웹 back-end 개발자 이고 정규직인 공고를 조회하고 아래와 같이 출력하세요.
-	@GetMapping("/find/complex")
+	@GetMapping("/complex")
 	@ResponseBody
 	public List<Recruit> selectPositionType(){
 		List<Recruit> recruitList = recruitRepository.findAllByPositionAndType("웹 back-end 개발자", "정규직");
 				
 		
+		return recruitList;
+	}
+	
+	// 4. 복합 조건 조회
+	// 정규직이거나 연봉이 9000 이상인 공고를 조회하고 아래와 같이 출력하세요.
+	@GetMapping("/complex-2")
+	@ResponseBody
+	public List<Recruit> selectTypeSalary(){
+		List<Recruit> recruitList = recruitRepository.findAllByTypeOrSalaryGreaterThanEqual("정규직", 9000);
+		
+		return recruitList;
+	}
+	
+	
+	//	5. 정렬 제한 조건
+	//	계약직 목록을 연봉 기준으로 내림차순 정렬해서 3개만 조회하세요.
+	@GetMapping("/sort")
+	@ResponseBody
+	public List<Recruit> sortTypeBySalary(){
+		List<Recruit> recruitList = recruitRepository.findAllTop3ByTypeOrderBySalaryDesc("계약직");
+		return recruitList;
+	}
+	
+	//	6. 범위 조회
+	//	성남시 분당구가 지역인 연봉 7000 이상 8500 이하인 공고를 조회하고 아래와 같이 출력하세요.
+	@GetMapping("/between")
+	@ResponseBody
+	public List<Recruit> findRegionSalary(){
+		List<Recruit> recruitList = recruitRepository.findAllByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
+		return recruitList;
+	}
+	
+	
+	//	7. Native query
+	//	마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요
+	@GetMapping("/query")
+	@ResponseBody
+	public List<Recruit> findTypeQuery(){
+		List<Recruit> recruitList = recruitRepository.findByTypeQuery("정규직");
 		return recruitList;
 	}
 	
--- src/main/java/com/uilangage/spring/test/jpa/repository/RecruitRepository.java
+++ src/main/java/com/uilangage/spring/test/jpa/repository/RecruitRepository.java
@@ -3,6 +3,8 @@ package com.uilangage.spring.test.jpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.uilangage.spring.test.jpa.domain.Recruit;
@@ -15,5 +17,16 @@ public interface RecruitRepository extends JpaRepository<Recruit, Integer> {
 	public List<Recruit> findAllBycompanyId(int companyId);
 	
 	public List<Recruit> findAllByPositionAndType(String position, String type);
+	
+	public List<Recruit> findAllByTypeOrSalaryGreaterThanEqual(String type, int salary);
+
+	public List<Recruit> findAllTop3ByTypeOrderBySalaryDesc(String type);
+	
+	public List<Recruit> findAllByRegionAndSalaryBetween(String region, int start, int end);
+	
+//	마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요
+	@Query(value="SELECT * FROM `recruit` WHERE `deadline` >= 2026-04-10 AND salary >= 8100 AND `type` = :type " , nativeQuery=true)
+	public List<Recruit> findByTypeQuery(@Param("type") String type);
+	
 	
 }
